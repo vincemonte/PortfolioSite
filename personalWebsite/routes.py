9c0dc029e217733732831cec9c6bf3dd755260e8
vincemonte <vincentmonteleone99@gmail.com>
2020-04-15 02:49:14 -0400

    Implemented the final SQLAlchemy tables, updated and added HTML to support the changes. New pages and routes were added to categorize the different types of projects and for each project to have their own page.

--- personalWebsite/routes.py
+++ personalWebsite/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect, request
 from personalWebsite import app, db
 from personalWebsite.forms import PostForm
 from personalWebsite.models import Project, HomePost
+from personalWebsite.utils import set_type_image
 
 
 @app.route('/')
@@ -11,17 +12,6 @@ def homepage():
     posts = HomePost.query.order_by(HomePost.id.desc()).paginate(page=page, per_page=6)
     return render_template('homepage.html', posts=posts)
 
-@app.route('/coding')
-def coding():
-    return render_template('coding.html')
-
-@app.route('/writings')
-def writings():
-    return render_template('writings.html')
-
-@app.route('/photography')
-def photography():
-    return render_template('photography.html')
 
 @app.route('/about')
 def about():
@@ -33,17 +23,20 @@ def project(project_id):
     post = Project.query.get_or_404(project_id)
     return render_template('project.html', post=post)
 
+@app.route('/coding', methods=['GET'])
+def coding():
+    posts = Project.query.filter_by(type='coding')
+    return render_template('coding.html', posts=posts)
 
-def set_type_image(form_type_image):
-    path = 'images/'
-    if form_type_image == 'coding':
-        fname = 'coding_project_type.png'
-    elif form_type_image == 'writing':
-        fname = 'writing_project_type.png'
-    else:
-        fname = 'photography_project_type.png'
-    path += fname
-    return path
+@app.route('/writings')
+def writings():
+    posts = Project.query.filter_by(type='writing')
+    return render_template('writings.html', posts=posts)
+
+@app.route('/photography')
+def photography():
+    posts = Project.query.filter_by(type='photography')
+    return render_template('photography.html', posts=posts)
 
 
 @app.route('/addpost', methods=['GET', 'POST'])
