ea1ac5bb4502e90ee2ea0baa0505f8c68c39289a
vincemonte <vincentmonteleone99@gmail.com>
2020-04-14 21:10:48 -0400

    Furthered the database implementation for a 1:1 relationship between projects and their home page posts.

--- personalWebsite/models.py
+++ personalWebsite/models.py
@@ -1,23 +1,22 @@
 from personalWebsite import db
 from datetime import datetime
-'''
-class Post():
+
+#Parent
+class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    type_image = db.Column(db.String(20), nullable=False)
-    content_synopsis = db.Column(db.Text, nullable=False)
-    content = db.Column(db.Text, nullable=True)
-    image = db.Column(db.String(20), nullable=True)
-    date_posted = db.Column(db.DateTiime, nullable=False, default=datetime.utcnow)
+    type = db.Column(db.String(20), nullable=False)
+    content= db.Column(db.Text, nullable=True)
+    images = db.Column(db.String(50), nullable=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    home_post = db.relationship('HomePost', backref='project', uselist=False)
     def __repr__(self):
         return f"Post('{self.id}','{self.title}', '{self.date_posted}')"
-'''
-
-class Post(db.Model):
+#Child
+class HomePost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    type_image = db.Column(db.String(20), nullable=False)
-    content = db.Column(db.Text, nullable=True)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), unique=True)
+    type_image = db.Column(db.String(30), nullable=False)
+    content = db.Column(db.Text, nullable=False)
     def __repr__(self):
-        return f"Post('{self.id}','{self.title}', '{self.date_posted}', '{self.type_image}')"
+        return f"Post('{self.id}','{self.project_id}', '{self.type_image}')"
--- personalWebsite/routes.py
+++ personalWebsite/routes.py
@@ -1,14 +1,15 @@
 from flask import render_template, url_for, flash, redirect, request
 from personalWebsite import app, db
 from personalWebsite.forms import PostForm
-from personalWebsite.models import Post
+from personalWebsite.models import Project, HomePost
+from personalWebsite.utils import set_type_image
 
 
 @app.route('/')
 @app.route('/home')
 def homepage():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=6)
+    posts = HomePost.query.order_by(HomePost.id.desc()).paginate(page=page, per_page=6)
     return render_template('homepage.html', posts=posts)
 
 @app.route('/coding')
@@ -27,24 +28,15 @@ def photography():
 def about():
     return render_template('about.html')
 
-def set_type_image(form_type_image):
-    path = 'static/images/'
-    if form_type_image == 'code':
-        fname = 'coding_project_type.png'
-    elif form_type_image == 'writing':
-        fname = 'writing_project_type.png'
-    else:
-        fname = 'photography_project_type.png'
-    path += fname
-    return path
-
 @app.route('/addpost', methods=['GET', 'POST'])
 def addpost():
     form = PostForm()
     if form.validate_on_submit():
-        type_image = set_type_image(form.type_image.data)
-        post = Post(title=form.title.data, type_image=type_image, content=form.content.data)
-        db.session.add(post)
+        project_post = Project(title=form.title.data, type=form.type.data, content=form.content.data, images=form.images.data)
+        type_image = set_type_image(form.type)
+        home_post = HomePost(type_image=type_image, content=form.synopsis.data, project=project_post)
+        db.session.add(project_post)
+        db.session.add(home_post)
         db.session.commit()
         flash('Post has been created.', 'success')
         return redirect(url_for('homepage'))
