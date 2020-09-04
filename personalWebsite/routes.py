1ea9e4c592e777577e9e26b00944a5d256818610
vincemonte <vincentmonteleone99@gmail.com>
2020-09-03 22:54:24 -0400

    Fully functional uploading system implemented and tested. Changes to CSS (mainly colors), as well as modifying utils.py appropriately to include all utility functions formerly in routes.py

--- personalWebsite/routes.py
+++ personalWebsite/routes.py
@@ -5,6 +5,7 @@ from personalWebsite import app, db
 from personalWebsite.forms import PostForm
 from personalWebsite.models import Project, HomePost
 from werkzeug.utils import secure_filename
+from personalWebsite.utils import save_file, save_project_files, get_project_files, set_type_image
 
 @app.route('/')
 @app.route('/home')
@@ -22,71 +23,34 @@ def about():
 @app.route('/project/<int:project_id>')
 def project(project_id):
     project_post = Project.query.get_or_404(project_id)
+    #still assigning files as so and potentially returning None makes this easier on the Jinga templating
     files = get_project_files(project_post)
     return render_template('project.html', project_post=project_post, files=files)
 
 @app.route('/coding', methods=['GET'])
 def coding():
-    posts = Project.query.filter_by(type='coding')
+    posts = Project.query.filter_by(type='coding').order_by(Project.id.desc())
     return render_template('coding.html', posts=posts)
 
 @app.route('/writings')
 def writings():
-    posts = Project.query.filter_by(type='writing')
+    posts = Project.query.filter_by(type='writing').order_by(Project.id.desc())
     return render_template('writings.html', posts=posts)
 
 @app.route('/photography')
 def photography():
-    posts = Project.query.filter_by(type='photography')
+    posts = Project.query.filter_by(type='photography').order_by(Project.id.desc())
     return render_template('photography.html', posts=posts)
 
 
-def set_type_image(form_type_image):
-    path = 'images/'
-    if form_type_image == 'code':
-        fname = 'coding_project_type.png'
-    elif form_type_image == 'writing':
-        fname = 'writing_project_type.png'
-    else:
-        fname = 'photography_project_type.png'
-    path += fname
-    return path
-
-def save_file(form_file, proj_dir_path):
-    fn = secure_filename(form_file.filename)
-    file_path = os.path.join(proj_dir_path, fn)
-    form_file.save(file_path)
-    return fn
-
-def save_project_files(form):
-    #create the relative path to make directory
-    rel_dir_path = os.path.join(app.config['UPLOAD_FOLDER'], form.title.data.strip().replace(" ", ""))
-    full_dir_path = os.path.join(app.root_path, 'static', rel_dir_path)
-    os.mkdir(full_dir_path)
-    for file in form.files.data:
-        save_file(file, full_dir_path)
-    return rel_dir_path
-
-'''
-Returns all files in the project's directory as a full file path
-Return type: list
-'''
-def get_project_files(project):
-    full_dir_path = os.path.join(app.root_path, 'static', project.files)
-    files = os.listdir(full_dir_path)
-    file_paths = []
-    for file in files:
-        # we simply join them here to avoid concatenation in HTML file
-        file_paths.append(os.path.join(project.files, file))
-    return file_paths
 
 @app.route('/addpost', methods=['GET', 'POST'])
 def addpost():
     form = PostForm()
     if form.validate_on_submit():
         project_post = Project(title=form.title.data, type=form.type.data, content=form.content.data)
-        if form.files.data:
-            # we assign the full path retuned from save_project_files function to the Project db object
+        if 'files' in request.files or 'file[]' in request.files: #if there are one or more in the post request...
+            # we assign the full path returned from save_project_files function to the Project db object
             project_post.files = save_project_files(form)
         type_image = set_type_image(form.type.data)
         home_post = HomePost(type_image=type_image, content=form.synopsis.data, project=project_post)
--- personalWebsite/utils.py
+++ personalWebsite/utils.py
@@ -1,6 +1,9 @@
+import os
+from personalWebsite import app
+
 def set_type_image(form_type_image):
     path = 'images/'
-    if form_type_image == 'code':
+    if form_type_image == 'coding':
         fname = 'coding_project_type.png'
     elif form_type_image == 'writing':
         fname = 'writing_project_type.png'
@@ -8,3 +11,35 @@ def set_type_image(form_type_image):
         fname = 'photography_project_type.png'
     path += fname
     return path
+
+def save_file(form_file, proj_dir_path):
+    fn = secure_filename(form_file.filename)
+    file_path = os.path.join(proj_dir_path, fn)
+    form_file.save(file_path)
+    return fn
+
+def save_project_files(form):
+    #create the relative path to make directory
+    rel_dir_path = os.path.join(app.config['UPLOAD_FOLDER'], form.title.data.strip().replace(" ", ""))
+    full_dir_path = os.path.join(app.root_path, 'static', rel_dir_path)
+    os.mkdir(full_dir_path)
+    for file in form.files.data:
+        save_file(file, full_dir_path)
+    return rel_dir_path
+
+'''
+Returns all files in the project's directory as a full file path
+Return type: list
+'''
+def get_project_files(project):
+    #we should only attempt to load image files if they exist
+    if project.files:
+        full_dir_path = os.path.join(app.root_path, 'static', project.files)
+        files = os.listdir(full_dir_path)
+        file_paths = []
+        for file in files:
+            # we simply join them here to avoid concatenation in HTML file
+            file_paths.append(os.path.join(project.files, file))
+            return file_paths
+    else:
+        return None
