5ffc3bf4b5ef6f12cd5948fa0309815db1037573
vincemonte <vincentmonteleone99@gmail.com>
2020-11-01 00:29:10 -0400

    Nav bar JS animation

--- personalWebsite/static/pageUtils.js
+++ personalWebsite/static/pageUtils.js
@@ -1,32 +1,25 @@
 window.addEventListener("scroll", resizeElementsOnScroll);
 
 function resizeElementsOnScroll() {
-  resizeJumbotron();
   resizeNavBar();
 }
 
-function resizeJumbotron(){
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-    document.getElementById("home-jumbotron").style.padding = "1.5em";
-  }
-  else{
-    document.getElementById("home-jumbotron").style.padding = "4em";
-  }
-}
 
 function resizeNavBar(){
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-    document.getElementById("navigation-bar").style.padding = "0";
-    document.getElementById("page-links").style.fontSize="16px";
-    var x = document.getElementById("logo");
+    document.getElementById("navigation-bar").style.padding = ".1em";
+    document.getElementById("nav-list-container").style.fontSize="19.3px";
+    var x = document.getElementById("navbar-logo");
     x.style.width = "40px";
     x.style.height = "40px";
+    document.getElementById("navbar-brand").style.fontSize="1.5em";
   }
   else{
     document.getElementById("navigation-bar").style.padding = ".5em";
-    document.getElementById("page-links").style.fontSize = "19px";
-    var x = document.getElementById("logo");
-    x.style.width = "60px";
-    x.style.height = "60px";
+    document.getElementById("nav-list-container").style.fontSize = "21px";
+    var x = document.getElementById("navbar-logo");
+    x.style.width = "65px";
+    x.style.height = "65px";
+    document.getElementById("navbar-brand").style.fontSize="1.8em";
   }
 }
