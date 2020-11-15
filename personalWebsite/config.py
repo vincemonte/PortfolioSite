e9b0398ac08c43c675d5ad19ef0d8be6fef430bb
vincemonte <vincentmonteleone99@gmail.com>
2020-11-15 00:27:12 +0000

    First commit from working dev on linux server.

--- personalWebsite/config.py
+++ personalWebsite/config.py
@@ -1,7 +1,13 @@
-import os
+import json
+import os 
+
+with open('/etc/config.json') as config_file:
+	config = json.load(config_file)
+
+
 class Config:
     #setting the secret key
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     #defining the upload folder path for easy file saving and loading
     UPLOAD_FOLDER = os.path.join('images', 'project_images')
