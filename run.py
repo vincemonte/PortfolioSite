79f8a5bf6e004457e8e5870fe0bc681eb8fc6b6a
vincemonte <vincentmonteleone99@gmail.com>
2021-09-12 03:10:04 -0400

    Slight bug

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from personalWebsite import create_test_app
+from personalWebsite import create_app
 
-app = create_test_app()
+app = create_app
 
 if __name__  == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
