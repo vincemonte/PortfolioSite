a5484f0936519798a79eb66f89ccbc16c74cf734
vincemonte <vincentmonteleone99@gmail.com>
2021-09-12 03:44:08 -0400

    Fixing crashed site.

--- personalWebsite/config.py
+++ personalWebsite/config.py
@@ -1,18 +1,14 @@
 import json
 import os
 
-deployed = False
 
-if os.path.isdir('/etc/config.json'):
-	with open('/etc/config.json') as config_file:
-		config = json.load(config_file)
-	deployed = True
+with open('/etc/config.json') as config_file:
+	config = json.load(config_file)
 
 class DeployConfig:
     #setting the secret key
-	if deployed is True:
-	    SECRET_KEY = config.get('SECRET_KEY')
-	    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+	SECRET_KEY = config.get('SECRET_KEY')
+	SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     #defining the upload folder path for easy file saving and loading
 	UPLOAD_FOLDER = os.path.join('images', 'project_images')
 
